8d7c66a7cd65c5d95cbc3d25a617142dcbd65721
Stellan Wetzig <stellanwetzig@yahoo.de>
2024-02-06 12:44:50 +0100

    solve react arrays users challenge

---
+++ react-with-arrays_users/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./styles.css";
+import Card from "./components/Card";
+import Title from "./components/Title";
+import { USERS } from "./db";
+
+export default function App() {
+  return (
+    <main className="app">
+      <Title text="ClientBoard" />
+      <div className="app__card-grid">
+        {USERS.map((user) => (
+          <Card key={user.id} user={user} />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+// <main className="app">
+// <Title text="ClientBoard" />
+// <div className="app__card-grid">
+//   {users.map((user) => (
+//     <Card
+//       key={user.id}
+//       name={user.name}
+//       roles={user.roles}
+//       about={user.about}
+//     />
+//   ))}
+// </div>
+// </main>
