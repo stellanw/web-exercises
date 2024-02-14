336f660a4e405c99cb2b9fa0fe936a3680e39985
Stellan Wetzig <stellanwetzig@yahoo.de>
2024-02-14 16:43:48 +0100

    refactored

--- react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
+++ react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const StyledBox = styled.div`
+export default styled.div`
   width: 100px;
   height: 100px;
   background-color: ${(props) => (props.$isBlack ? "black" : "green")};
@@ -11,6 +11,6 @@ const StyledBox = styled.div`
   }
 `;
 
-export default function BoxWithStyledComponents({ $isBlack }) {
-  return <StyledBox $isBlack={$isBlack}></StyledBox>;
-}
+// export default function BoxWithStyledComponents({ $isBlack }) {
+//   return <StyledBox $isBlack={$isBlack}></StyledBox>;
+// }
